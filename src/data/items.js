b289f81b9371eca9b7e53ebdc811219ef02083b1
Jordyn Doanne <wf6d5x7hjx@privaterelay.appleid.com>
2026-05-27 22:37:43 -0700

    Add artwork data

---
+++ src/data/items.js
@@ -0,0 +1,13 @@
+export const items = [
+  {
+    id: 'welcome',
+    title: 'Welcome',
+    palette: ['#FFF4D6', '#F0D28B', '#D99B4E', '#8A5A2B'],
+    blocks: [
+      { id: 'a', color: 0, x: 0, y: 0, w: 360, h: 120 },
+      { id: 'b', color: 1, x: 130, y: 20, w: 100, h: 90 },
+      { id: 'c', color: 2, x: 90, y: 130, w: 180, h: 190 },
+      { id: 'd', color: 3, x: 120, y: 70, w: 120, h: 80 }
+    ]
+  }
+];
