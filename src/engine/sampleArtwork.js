0cac7593db547fcea44238d6cf73e76297938c58
Jordyn Doanne <wf6d5x7hjx@privaterelay.appleid.com>
2026-05-27 22:40:23 -0700

    Add sample SVG artwork for coloring engine

---
+++ src/engine/sampleArtwork.js
@@ -0,0 +1,15 @@
+export const sampleArtwork = {
+  id: 'welcome-jesus-sample',
+  title: 'Welcome',
+  width: 360,
+  height: 420,
+  palette: ['#FFF4D6', '#F0D28B', '#D99B4E', '#8A5A2B', '#FFFDF7', '#7C97B8'],
+  regions: [
+    { id: 'sky', colorIndex: 0, path: 'M0 0 H360 V130 H0 Z' },
+    { id: 'halo', colorIndex: 1, path: 'M110 20 H250 V140 H110 Z' },
+    { id: 'robe', colorIndex: 2, path: 'M75 170 H285 V390 H75 Z' },
+    { id: 'hair', colorIndex: 3, path: 'M115 80 H245 V175 H115 Z' },
+    { id: 'face', colorIndex: 4, path: 'M140 95 H220 V175 H140 Z' },
+    { id: 'ground', colorIndex: 5, path: 'M0 340 H360 V420 H0 Z' }
+  ]
+};
